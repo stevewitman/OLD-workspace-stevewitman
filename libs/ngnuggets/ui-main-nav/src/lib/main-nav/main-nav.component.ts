c999caba0fc8699f30c13415355f73bab91ee1df
Steve Witman <stevewitman@gmail.com>
2021-04-24 13:42:13 -0600

    experiment nested menu with animation

--- libs/ngnuggets/ui-main-nav/src/lib/main-nav/main-nav.component.ts
+++ libs/ngnuggets/ui-main-nav/src/lib/main-nav/main-nav.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { animate, group, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'ng-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({ height: '*', opacity: 0 })),
+      transition(':leave', [
+        style({ height: '*', opacity: 1 }),
+
+        group([
+          animate(300, style({ height: 0 })),
+          animate('200ms ease-in-out', style({ opacity: '0' })),
+        ]),
+      ]),
+      transition(':enter', [
+        style({ height: '0', opacity: 0 }),
+
+        group([
+          animate(300, style({ height: '*' })),
+          animate('400ms ease-in-out', style({ opacity: '1' })),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class MainNavComponent {
   isHandset$: Observable<boolean> = this.breakpointObserver
@@ -16,5 +39,11 @@ export class MainNavComponent {
       shareReplay()
     );
 
+  showFilters = false;
+
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  toggleShowFilters() {
+    this.showFilters = !this.showFilters;
+  }
 }
