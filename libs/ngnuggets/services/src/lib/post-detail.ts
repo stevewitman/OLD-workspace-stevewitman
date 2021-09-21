71b865d04d46f046cb710da1e8e49aea6cda07cc
Steve Witman <stevewitman@gmail.com>
2021-09-21 11:41:55 -0600

    ngnuggetsdotcom: update post-detail interface and add more temp posts

--- libs/ngnuggets/services/src/lib/post-detail.ts
+++ libs/ngnuggets/services/src/lib/post-detail.ts
@@ -1,12 +1,11 @@
 export interface PostDetail {
   id: string;
   slug: string; //                  2021-09-01-A
-  contentType: 'video' | 'blog' | 'podcast' | 'question' | 'community' | 'meetup' | 'conference' | 'poll'; 
+  contentType: 'video' | 'blog' | 'podcast' | 'community'; 
   duration?: string;
   contentTitle: string;
   contentUrl: string; 
   contentDescription: string;
-  descriptionBy: string; //         either from source, by submitter, or by ADMIN
   imgUrl: string;
   datePosted: string;
   dateCreated?: string;
@@ -15,12 +14,12 @@ export interface PostDetail {
   sourceUrl?: string; //            'https://www.youtube.com/'
   authorName?: string; //           Frontend Decoded, Angular University,
   authorUrl?: string; //            channel url, personal website, etc.
-  addedBy?: string; //              USER who submitted the article
   showAddedBy?: true;
   twitter?: string;
-  stars?: number;
+  likes?: number;
   featured: boolean;
-  saved?: number;
   tags?: string[];
   visits?: number;
+  status: 'wait' | 'block' | 'show',
+  notes?: ''
 }
